96809da49b4c40bd8d9d302096d1ff9ceeb6acc0
feedbackex <feedbackex@gmail.com>
2015-09-22 01:19:48 +0300

    BaseScripts
    Base CryEngine 3.5.8 Scripts

---
+++ Entities/Physics/JointGen.lua
@@ -0,0 +1,30 @@
+JointGen = {
+	Properties = {
+		object_BreakTemplate = "objects/broken_cube.cgf",
+		file_Material = "",
+		bForceEntityPieces = 0,
+		bHierarchical = 0,
+		Impulse = 1000,
+		radius = 0.05,
+		scale = 1,
+		offset = {x=0,y=0,z=0},
+	},
+
+	Editor = {
+		Icon = "Shake.bmp",
+		DisplayArrow = 1,
+	},
+}
+
+function JointGen:OnReset()
+	local ang = {x=0,y=0,z=0};
+	self:SetAngles(ang);
+end
+
+function JointGen:OnPropertyChange()
+	self:OnReset()
+end
+
+function JointGen:OnSpawn()
+	self:OnReset()
+end
